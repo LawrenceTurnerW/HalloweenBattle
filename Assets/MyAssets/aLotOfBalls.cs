1c1088586cc9457fb7f487389dfa5c7891c1519c
LawrenceTurnerW <watawata.takatan@gmail.com>
2023-10-24 01:40:19 +0900

    ボールを生成するサンプルを追加

---
+++ Assets/MyAssets/aLotOfBalls.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class TestScript : MonoBehaviour
+{
+    // ここで指定したオブジェクトの位置や角度を基準にする
+    [SerializeField] private GameObject controlPoint;
+
+    // 入力ボタンフィールをを作成できる
+    [SerializeField] private OVRInput.RawButton inputBtn;
+
+    // 生成するオブジェクト
+    [SerializeField] private GameObject ball;
+
+    private void Update()
+    {
+        // 指定したボタンが押されたら
+        if (OVRInput.GetDown(inputBtn))
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                // ballをcontrolPointの位置と角度に合わせて生成する
+                Instantiate(ball, controlPoint.transform.position, Quaternion.identity);
+            }
+        }
+    }
+}
