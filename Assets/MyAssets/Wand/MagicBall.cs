263a4a4c35b274f626e30bb1c93ebae9ed63231d
LawrenceTurnerW <watawata.takatan@gmail.com>
2023-10-26 01:32:57 +0900

    かぼちゃとお化けが触れた場合に消えるように
    お互いに消滅する

--- Assets/MyAssets/Wand/MagicBall.cs
+++ Assets/MyAssets/Wand/MagicBall.cs
@@ -1,4 +1,6 @@
 using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
 
 // 発射されるかぼちゃ
 public class MagicBall : MonoBehaviour
@@ -6,5 +8,19 @@ public class MagicBall : MonoBehaviour
     private void Start()
     {
         // TODO：生成して暫くしたら球を削除する
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        // 衝突したオブジェクトがおばけだった場合
+        // TODO:Conatainsで検索するのを修正する
+        if (collision.gameObject.name.Contains("ghost"))
+        {
+            // まずおばけを削除
+            Destroy(collision.gameObject);
+
+            //その後自身を削除
+            Destroy(this.gameObject);
+        }
     }
 }
