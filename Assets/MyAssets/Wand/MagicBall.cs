9e7f9d3a66ef599db7df386af4d860771c976aa3
LawrenceTurnerW <watawata.takatan@gmail.com>
2023-10-26 02:37:04 +0900

    おばけが破壊されたらお菓子が出現するように

--- Assets/MyAssets/Wand/MagicBall.cs
+++ Assets/MyAssets/Wand/MagicBall.cs
@@ -5,6 +5,14 @@ using System.Collections.Generic;
 // 発射されるかぼちゃ
 public class MagicBall : MonoBehaviour
 {
+    // 生成するお菓子オブジェクト
+    [SerializeField] private GameObject sweet1;
+    [SerializeField] private GameObject sweet2;
+    [SerializeField] private GameObject sweet3;
+    [SerializeField] private GameObject sweet4;
+    [SerializeField] private GameObject sweet5;
+    [SerializeField] private GameObject sweet6;
+
     private void Start()
     {
         // TODO：生成して暫くしたら球を削除する
@@ -16,11 +24,33 @@ public class MagicBall : MonoBehaviour
         // TODO:Conatainsで検索するのを修正する
         if (collision.gameObject.name.Contains("ghost"))
         {
+            CreateSweet(sweet1);
+            CreateSweet(sweet2);
+            CreateSweet(sweet3);
+            CreateSweet(sweet4);
+            CreateSweet(sweet5);
+            CreateSweet(sweet6);
             // まずおばけを削除
             Destroy(collision.gameObject);
 
             //その後自身を削除
             Destroy(this.gameObject);
         }
+    }
+
+
+    private void CreateSweet(GameObject sweet)
+    {
+        //スイーツを生成
+        GameObject _sweet = Instantiate(sweet, this.transform.position, this.transform.rotation);
+
+        // ランダムな方向に力をかけて跳ねさせる
+        float rnd = Random.Range(-3, 3);
+
+        Rigidbody rb = _sweet.GetComponent<Rigidbody>();
+        rb.AddForce(new Vector3(rnd, Mathf.Abs(rnd) * 3, rnd), ForceMode.Impulse);// 上方向には跳ねてほしいので絶対値をかけた上で値を大きくする
+
+        //その3秒後に自身を削除
+        Destroy(_sweet.gameObject, 3f);
     }
 }
