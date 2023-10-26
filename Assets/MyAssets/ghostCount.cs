37ddb6fa0f73804e3ae958ad8b154406037d0fdf
LawrenceTurnerW <watawata.takatan@gmail.com>
2023-10-27 03:58:01 +0900

    おばけ一定数以下の場合自動で出現するように

--- Assets/MyAssets/ghostCount.cs
+++ Assets/MyAssets/ghostCount.cs
@@ -1,9 +1,15 @@
 using UnityEngine;
-
-public class Timer : MonoBehaviour
+using System.Collections;
+public class ghostCount : MonoBehaviour
 {
+    // 発射地点
+    [SerializeField] private GameObject centerEye;
 
-    [SerializeField] private float span = 5f;
+    // 間隔
+    [SerializeField] private float span = 3f;
+
+    // おばけ
+    [SerializeField] private GameObject ghost;
 
     void Start()
     {
@@ -15,7 +21,26 @@ public class Timer : MonoBehaviour
         while (true)
         {
             yield return new WaitForSeconds(span);
-            GameObject[] cubeObjects = GameObject.FindGameObjectsWithTag("Cube");
+            GameObject[] ghosts = GameObject.FindGameObjectsWithTag("ghost");
+
+            Vector3 playerPos = centerEye.transform.position;
+
+            foreach (GameObject ghost in ghosts)
+            {
+                float dis = Vector3.Distance(ghost.transform.position, playerPos);
+                // 10m以上は多分メッシュ貫通してどっか行ってるのでおばけを消す
+                if (dis > 10)
+                {
+                    Destroy(ghost);
+                }
+            }
+            int rnd = Random.Range(-10, 10);
+            if (ghosts.Length < 10)
+            {
+                GameObject _newGhost = Instantiate(ghost, playerPos, Quaternion.identity);
+                Rigidbody rb = _newGhost.GetComponent<Rigidbody>();
+                rb.AddForce(new Vector3(rnd, rnd, rnd) * 50, ForceMode.Force);
+            }
         }
     }
 }
